ef5f4410ab04cb1b9f5a24005fc7c173726417a9
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-06-06 05:44:27 +0200

    Add files via upload

---
+++ Ejercicio3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un algoritmo que imprima las sumas de los 100 primeros números enteros
+            for (int i = 1; i <=100; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Hacer un algoritmo que imprima los números pares entre el 0 al 100
+            for (int i = 0; i <=100; i++)
+            {
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+         }
+    }
+}
