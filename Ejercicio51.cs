9f7a7c87a0947de5485050430d48cb7d08414afe
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-06-06 07:44:14 +0200

    Add files via upload

---
+++ Ejercicio51.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escriba un programa que lea dos enteros y que determine e imprima si el primero es multiplo del segundo
+
+            int n1 = 0, n2 = 0;
+            Console .Write("Ingrese el primer valor: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console .Write("Ingrese el segundo valor: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            if (n2 % n1 == 0)
+            {
+                Console .WriteLine("El primer número leídos es múltiplo del segundo");
+            }
+            else
+            {
+                Console .WriteLine("No es múltiplo del segundo número leído");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Ejercicio52.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Escriba un programa que calcule los cuadrados y los cubos de los números del 1 al 10,
+            //imprimiendolos en la siguiente forma.
+
+            int c = 0;
+            Console.WriteLine("Número           Cuadrado            Cubo");
+            while (c < 10)
+            {
+                c++;
+                Console.Write("\n");
+                Console.Write("    " + c);
+                Console.Write("\t\t   " + (c * c));
+                Console.Write("\t\t\t" + c * c * c);
+
+            }
+            Console.ReadKey();
+        }
+        }
+}
