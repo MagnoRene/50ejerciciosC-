3f5ff98ad8adeba704bee8c0928f5192e2e4d998
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-06-06 05:58:31 +0200

    Add files via upload

---
+++ Ejercicio18.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimir y contar los múltiplos de 3 desde la unidad hasta un número que introducimos por teclado
+            int numero,cantidad;
+            numero = 0;
+            cantidad = 0;
+            Console.Write("Ingrese el numero donde desea Finalizar: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < numero; i++)
+            {
+                if (i%3==0)
+                {
+                    Console.WriteLine(i);
+                    cantidad = cantidad + 1;
+                }
+            }
+            Console.WriteLine("Cantidad de multiplos de 3 encontrados: {0} ",cantidad);
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ Ejercicio19.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Hacer un algoritmo que imprima los números del 1 al 100 .que calcule la suma de
+            //todos los números pares por un lado y otro la de todos los impares.
+            int cantPares, cantimpares;
+            cantPares = 0;
+            cantimpares = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+                if (i%2==0)
+                {
+                    cantPares = cantPares + i;
+                }
+                else
+                {
+                    cantimpares = cantimpares + i;
+                }
+            }            Console.WriteLine("Suma de numeros PARES: {0}", cantPares);            Console.WriteLine("Suma de numeros IMPARES: {0}", cantimpares);            Console.ReadKey();
+        }
+    }
+}
