2ab4b95ff4d6f77cd9544ed19f4bf401bcbe6130
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-06-06 04:25:59 +0200

    Add files via upload

---
+++ Ejercicio2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Hacer un algoritmo que imprima los números del 100 al 0 en orden decreciente
+            for (int i = 100; i >=0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
