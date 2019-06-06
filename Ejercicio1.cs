b9b4ba9e6b3d27e43cef3f3fa34a17fe572ff057
MagnoRene <51423734+MagnoRene@users.noreply.github.com>
2019-06-06 04:24:35 +0200

    Add files via upload

---
+++ Ejercicio1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que imprima los numeros del O al 100
+            for (int i = 0; i <=100; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
